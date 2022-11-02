047c68041b216f8c3ffeb712d06b9e6e624ce459
PaulTA91 <parmst202caledonian.ac.uk>
2022-11-02 10:42:10 +0000

    up to page 11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navigation from "./nav";
+import "./theme.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="theme1">
+        <div className="container">
+          <div className="left">
+            <Navigation />
+          </div>
+          <div className="right">
+            <button className="button">Button</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
